6f867cb9050831565273481c1b6cf7461b08d5ea
AustinLynes <austinlynes94@gmail.com>
2019-12-18 18:43:48 -0800

    added inital auth router, set up to start test driven development

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-const server = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const server = express()
+const authRouter = require('./routes/auth/auth-router')
+
+// --middleware--
+server.use(helmet())
+server.use(morgan('combined'))
+server.use(express.json())
+server.use(cors())
+
+server.use('/auth', authRouter)
+
+module.exports = server
