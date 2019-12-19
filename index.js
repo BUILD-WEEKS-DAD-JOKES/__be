01d05407f182e5298c817375b351a023b05bfc9d
AustinLynes <austinlynes94@gmail.com>
2019-12-18 20:34:29 -0800

    added a base route for server to see if deployment is going to work

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const server = require('./server')
 
 const PORT = process.env.PORT
 
+server.get('/', (req,res)=>{
+    res.send(`
+    <div>
+        <h1>Welcome to DadJokes Api</h1>
+        <p>author: Austin Lynes</p>
+    </div>
+    `)
+})
+
 server.listen(PORT, ()=>{
     console.log(`********//   API OPEN ON PORT ${PORT}  //********`)
 })
